276678fbd513cbe065050fde68894fc27353704b
afghl <airport.0930@gmail.com>
2017-10-15 16:07:11 +0800

    change settings

--- webapi/src/main/java/com/afghl/restaurant/webapi/HelloWorldFacade.java
+++ webapi/src/main/java/com/afghl/restaurant/webapi/HelloWorldFacade.java
@@ -1,4 +1,4 @@
-package com.afghl.restaurant.facade;
+package com.afghl.restaurant.webapi;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
