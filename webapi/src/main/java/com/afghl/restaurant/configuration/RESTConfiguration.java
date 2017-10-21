79bc444b30f18ca798425636b602a361dbb81c33
afghl <airport.0930@gmail.com>
2017-10-21 15:53:33 +0800

    add rest dependencies and add path prefix

---
+++ webapi/src/main/java/com/afghl/restaurant/configuration/RESTConfiguration.java
@@ -0,0 +1,36 @@
+package com.afghl.restaurant.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
+import org.springframework.web.servlet.View;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.BeanNameViewResolver;
+import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
+
+@Configuration
+public class RESTConfiguration {
+    @Bean
+    public View jsonTemplate() {
+        MappingJackson2JsonView view = new MappingJackson2JsonView();
+        view.setPrettyPrint(true);
+        return view;
+    }
+
+    @Bean
+    public ViewResolver viewResolver() {
+        return new BeanNameViewResolver();
+    }
+
+    @Bean
+    public RepositoryRestConfigurer repositoryRestConfigurer() {
+        return new RepositoryRestConfigurerAdapter() {
+            @Override
+            public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+                config.setBasePath("/api");
+            }
+        };
+    }
+}
--- webapi/src/main/java/com/afghl/restaurant/configuration/SessionFactoryConfig.java
+++ webapi/src/main/java/com/afghl/restaurant/configuration/SessionFactoryConfig.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SessionFactoryConfig {
-
     @Bean
     public SessionFactory sessionFactory(HibernateEntityManagerFactory hemf) {
         System.out.printf("we are in sessionFactory");
